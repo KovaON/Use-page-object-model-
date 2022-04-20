623cd2ca294743c89fcccf5ab8bf760755561b03
KovaON <kov.anton94@gmail.com>
2022-04-20 19:13:43 +0500

    Изменил файл conftest.py

--- conftest.py
+++ conftest.py
@@ -3,12 +3,22 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
+def pytest_addoption(parser):
+    """ В терминал подаем параметр вида '--language="es"'
+    По умолчанию передается параметр с английским языком
+    """
+    parser.addoption('--language', action='store', default='en', help='Choose language')
+
+
 @pytest.fixture(scope="function")
-def browser():
+def browser(request):
+    # задаем user_language
+    user_language = request.config.getoption('language')
+    # Создаем опции браузера
     options = Options()
+    # В опции вебдрайвера передаем user_language
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
-    return browser
+    browser.implicitly_wait(10)
     yield browser
-    print("\nquit browser..")
     browser.quit()
